1df17bead59b42a74cc64ca4e26aefd96bff4c22
hari-cat <heeman8374@gamil.com>
2026-06-06 22:37:48 +0900

    feat: herosection 수정

--- components/refrigerator/HeroSection.tsx
+++ components/refrigerator/HeroSection.tsx
@@ -1,24 +1,26 @@
 export function HeroSection() {
   return (
-    <section className="relative h-[350px]">
+    <section className="relative h-[300px]">
       <img
         src="/images/hero_banner.png"
-        alt=""
-        width={50}
-        height={50}
+        alt="hero section img"
+        width={70}
+        height={70}
         className="absolute inset-0 h-full w-full object-cover"
       />
 
       <div className="absolute inset-0 bg-black/20" />
 
-      <div className="relative px-10 pt-32 text-white">
+      <div className="relative px-10 pl-6 pt-25 text-white">
         <h1 className="text-3xl font-bold leading-tight">
           신선함은 그대로
           <br />
           관리는 단순하게
         </h1>
 
-        <p className="mt-6 text-xl">우리집냉장고로 신선함을 유지하세요</p>
+        <p className="mt-3 text-xs font-bold">
+          우리집냉장고로 신선함을 유지하세요
+        </p>
       </div>
     </section>
   );
