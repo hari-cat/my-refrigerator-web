0f002036cefa39370641f0bed3e59a63ecca5722
hari-cat <heeman8374@gamil.com>
2026-06-06 10:01:39 +0900

    feat: page 초기화

---
+++ app/Refreigerator.tsx
@@ -0,0 +1,7 @@
+export default function Refreigerator() {
+    return (
+        <main className="flex-1 flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
+        </main>
+    );
+}
