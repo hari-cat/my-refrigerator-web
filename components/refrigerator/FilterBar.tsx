7d373bf9f88801e44e0c89b2c9c0fc3788ae987f
hari-cat <heeman8374@gamil.com>
2026-06-06 23:13:52 +0900

    fix: key오류 수정

--- components/refrigerator/FilterBar.tsx
+++ components/refrigerator/FilterBar.tsx
@@ -7,18 +7,16 @@ export function FilterBar() {
     <div className="flex gap-3 overflow-x-auto px-4 py-4">
       {filterItems.map((item, idx) => {
         return (
-          <>
-            <button
-              key={`filter-items-${idx}`}
-              className={cn(
-                "flex items-center gap-1 whitespace-nowrap rounded-full border px-3 py-2 text-xs font-bold",
-                idx === 0 && "bg-gray-200",
-              )}
-            >
-              {item}
-              <img src="/images/down_arrow.png" alt="" width={8} height={8} />
-            </button>
-          </>
+          <button
+            key={`filter-items-${idx}`}
+            className={cn(
+              "flex items-center gap-1 whitespace-nowrap rounded-full border px-3 py-2 text-xs font-bold",
+              idx === 0 && "bg-gray-200",
+            )}
+          >
+            {item}
+            <img src="/images/down_arrow.png" alt="" width={8} height={8} />
+          </button>
         );
       })}
     </div>
