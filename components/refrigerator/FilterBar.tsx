8481c10e96b3a89146ef14bcf9839902425caad9
hari-cat <heeman8374@gamil.com>
2026-06-06 23:40:53 +0900

    feat: foodCard 클릭 동작 수정

--- components/refrigerator/FilterBar.tsx
+++ components/refrigerator/FilterBar.tsx
@@ -1,8 +1,19 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export function FilterBar() {
-  const filterItems = ["전체", "신선식품", "소비기한", "보관위치"];
+  const filterItems = ["전체"];
+  const [selectedItem, setSelectedItem] = useState("전체");
   // font-semibold
+
+  /**
+   * 필터 선택 함수
+   */
+  const handleSelectedItem = (value: string) => {
+    setSelectedItem(value);
+  };
+
   return (
     <div className="flex gap-3 overflow-x-auto px-4 py-4">
       {filterItems.map((item, idx) => {
@@ -11,8 +22,9 @@ export function FilterBar() {
             key={`filter-items-${idx}`}
             className={cn(
               "flex items-center gap-1 whitespace-nowrap rounded-full border px-3 py-2 text-xs font-bold",
-              idx === 0 && "bg-gray-200",
+              item === selectedItem && "bg-gray-200",
             )}
+            onClick={() => handleSelectedItem(item)}
           >
             {item}
             <img src="/images/down_arrow.png" alt="" width={8} height={8} />
